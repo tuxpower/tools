5a5c868ba1f22582606b6688ec97af40c257a89d
Jose Gaspar <jad.gaspar@gmail.com>
2018-06-26 19:49:52 +0100

    add comments and code refactoring

--- main.rb
+++ main.rb
@@ -27,49 +27,43 @@ end
 # Get an Amazon EC2 resource
 ec2 = Aws::EC2::Resource.new(region: 'eu-west-1')
 
+# Get an Amazon EC2 client
+client = Aws::EC2::Client.new
+
 instance = ec2.instance(instance_id)
 
 if instance.exists?
   puts "Root Device: #{instance.root_device_name}"
 end
 
-puts "AZ: #{instance.placement.availability_zone}"
-puts "Subnet: #{instance.subnet_id}"
-
-volumes_result = instance.volumes({
+volumes = instance.volumes({
   filters: [
     {
-			name: "attachment.device",
-			values: [instance.root_device_name], 
+      name: "attachment.device",
+      values: [instance.root_device_name],
     },
   ],
 })
 
-vol = ""
+volume = ""
 
-volumes_result.each do |volume|
-  puts "=" * 10
-  puts "Volume ID: #{volume.volume_id}"
-  vol = volume.volume_id
+volumes.each do |vol|
+  puts "Volume ID: #{vol.volume_id}"
+  volume = vol.volume_id
 end
 
-puts "Vol ID: #{vol}"
-
+puts "Stopping instance ID #{instance_id}"
 instance.stop
 
-client = Aws::EC2::Client.new
+puts "Waiting for instance ID #{instance_id} to stop"
+wait_for_instances(client, :instance_stopped, [instance_id])
 
-begin
-  client.wait_until(:instance_stopped, instance_ids:[instance_id])
-  puts "instance stopped"
-rescue Aws::Waiters::Errors::WaiterFailed => error
-  puts "failed waiting for instance stopping: #{error_message}"
-end
-
+puts "Detaching volume #{volume}"
 instance.detach_volume({
-  volume_id: vol,
+  volume_id: volume,
 })
 
+puts "Launching worker instance..."
 resp = client.run_instances({
   image_id: "ami-ca0135b3",
   instance_type: "t2.micro",
@@ -83,14 +77,27 @@ resp = client.run_instances({
 })
 
 worker_instance = ec2.instance(resp.instances[0].instance_id)
+puts "Waiting for worker instance ID #{worker_instance.instance_id} to be running"
 wait_for_instances(client, :instance_running, [worker_instance.instance_id])
 
-puts "Worker ID: #{worker_instance.instance_id}"
-
+puts "Attaching volume to worker instance"
 worker_instance.attach_volume({
-	device: "/dev/xvdz",
-	volume_id: vol,
+  device: "/dev/xvdz",
+  volume_id: volume,
 })
 
+puts "Terminating worker instance"
 worker_instance.terminate
+puts "Waiting for worker instance ID #{worker_instance.instance_id} to be terminated"
 wait_for_instances(client, :instance_terminated, [worker_instance.instance_id])
+
+puts "Attaching volume back to original instance"
+instance.attach_volume({
+  device: instance.root_device_name,
+  volume_id: volume,
+})
+
+puts "Starting original instance"
+instance.start
+puts "Waiting for original instance ID #{instance_id} to be running again"
+wait_for_instances(client, :instance_running, [instance_id])
