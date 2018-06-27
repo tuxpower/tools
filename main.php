1b4ee35e43f4312c4e1b1d36b0a11287781766a2
Jose Gaspar <jad.gaspar@gmail.com>
2018-06-27 16:39:37 +0100

    WIP

--- main.php
+++ main.php
@@ -9,7 +9,19 @@ $ec2Client = new Ec2Client([
     'profile' => 'default'
 ]);
 
+$instance = $argv[1];
+
 $result = $ec2Client->describeInstances();
 
-var_dump($result);
+var_dump($result['Reservations'][0]['Instances'][0]['BlockDeviceMappings'][0]['DeviceName']);
+
+$result = $ec2Client->describeInstanceAttribute([
+    'Attribute' => 'rootDeviceName',
+    'InstanceId' => $instance,
+]);
+
+//var_dump($result);
+print($result->get('RootDeviceName')['Value']."\n");	// using the get() method of the php model
+print($result['RootDeviceName']['Value']."\n");		// accessing the result like an associative array
+print($result->search('RootDeviceName.Value')."\n");	// executing JMESPath expression on the result data using the search() method
 ?>
